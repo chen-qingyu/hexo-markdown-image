c0412cf53778ac830c12e0a4a5566583f3efff70
chen-qingyu <236521827@qq.com>
2024-06-24 11:51:48 +0800

    handle current path

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 'use strict';
 
 hexo.extend.filter.register('before_post_render', function (data) {
-    // ![label](folder/filename.png) -> ![label](filename.png)
     data.content = data.content.replace(/!{1}\[([^\[\]]*)\]\((\S*)\s?(?:".*")?\)/g,
         function (match_str, label, path) {
             const parts = path.split('/');
+            if (parts.length == 3 && parts[0] == '.') {
+                parts.shift();
+            }
             if (parts.length == 2) {
                 const title = data.content.match(/title\:\s.+/g)[0].replace('title: ', '');
                 if (parts[0] == title) {
-                        return `![${label}](${parts[1]})`;
-                    }
-                return match_str;
-            } else {
-                console.error(`unresolved image path: ${path}`);
-                return match_str;
+                    return `![${label}](${parts[1]})`;
+                }
             }
+            return match_str;
         });
 
     return data;
