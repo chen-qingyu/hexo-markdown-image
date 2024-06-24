21787fb1f573a1d487d2abb5606c595709b17487
chen-qingyu <236521827@qq.com>
2024-06-24 11:14:58 +0800

    use const

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ hexo.extend.filter.register('before_post_render', function (data) {
     // ![label](folder/filename.png) -> ![label](filename.png)
     data.content = data.content.replace(/!{1}\[([^\[\]]*)\]\((\S*)\s?(?:".*")?\)/g,
         function (match_str, label, path) {
-            let parts = path.split('/');
+            const parts = path.split('/');
             if (parts.length == 2) {
-                let title = content.match(/title\:\s.+/g)[0].replace('title: ', '');
+                const title = content.match(/title\:\s.+/g)[0].replace('title: ', '');
                 if (parts[0] == title) {
                         return `![${label}](${parts[1]})`;
                     }
